1db2baed46c50f466c51b9b7657f7e43704481d8
Frederic Thomson <frederic@neoito.com>
2023-11-24 11:06:35 +0530

    [commitHash:b7431c2]

--- src/remote/federated_components/Button/Button.js
+++ src/remote/federated_components/Button/Button.js
@@ -1,20 +1,59 @@
-import React from 'react'
-import { useFederatedComponent } from '@appblocks/js-sdk'
+import React from "react";
+import LoaderImg from "../../assets/images/loader.png";
 
-const Button = (props) => {
-  const { Component: Button, errorLoadingButton } = useFederatedComponent(
-    process.env.BB_AUTH_ELEMENTS_URL,
-    'remotes',
-    './button_container',
-    React
-  )
+const Button = ({
+  name,
+  className,
+  type,
+  Loader,
+  isLoading,
+  disabled,
+  handleButton,
+  btnType,
+  disableOnValidate,
+  icon,
+  moreClass,
+}) => {
+  let buttonClass =
+    "flex justify-center items-center w-full min-h-[48px] min-w-[128px] rounded-5 text-base font-bold leading-5 px-4 hover:opacity-80 whitespace-nowrap";
+  if (className) {
+    buttonClass = className;
+  }
+
+  if (disabled || disableOnValidate || isLoading) {
+    buttonClass += " bg-gray-variant-2 text-white cursor-not-allowed";
+  } else {
+    if (btnType === "primary") {
+      buttonClass += " bg-primary text-white";
+    } else if (btnType === "secondary") {
+      buttonClass += " bg-white border border-black text-black";
+    } else if (btnType === "terinary") {
+      buttonClass += " bg-white border-none  text-black";
+    } else if (btnType === "info") {
+      buttonClass += " bg-info border-none  text-white";
+    }
+  }
   return (
-    <React.Suspense fallback={''}>
-      {errorLoadingButton
-        ? `Error loading module "${module}"`
-        : Button && <Button {...props} />}
-    </React.Suspense>
-  )
-}
+    <button
+      className={buttonClass + " " + moreClass}
+      type={type}
+      disabled={disabled || isLoading}
+      onClick={() => {
+        if (!disableOnValidate && type === "button") {
+          handleButton();
+        }
+      }}
+    >
+      {icon ? (
+        <img className="mr-[14px] w-[14px]" src={icon} alt="btn_icon" />
+      ) : null}
+      {isLoading
+        ? Loader || (
+            <img src={LoaderImg} className="w-[26px] infinite-rotate flex" />
+          )
+        : name}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
